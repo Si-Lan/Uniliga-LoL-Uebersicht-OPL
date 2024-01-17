ea6c057c3caf764c77a5798292f5c5c55410710c
Si-Lan <online@simon-lang.de>
2024-01-17 13:44:14 +0100

    fixed game assign for playoffs

--- admin/functions/get-rgapi-data.php
+++ admin/functions/get-rgapi-data.php
@@ -330,7 +330,7 @@ function assign_and_filter_game($RiotMatchID,$tournamentID):array {
 
 	// check from which match the game is
 	$matchDB = [];
-	if ($tournament["eventType"] == "group") {
+	if ($tournament["eventType"] == "group" || $tournament["eventType"] == "playoffs") {
 		$matchDB = $dbcn->execute_query("SELECT * FROM matchups WHERE ((OPL_ID_team1 = ? AND OPL_ID_team2 = ?) OR (OPL_ID_team1 = ? AND OPL_ID_team2 = ?)) AND OPL_ID_tournament = ?", [$BlueTeamID, $RedTeamID, $RedTeamID, $BlueTeamID, $tournamentID])->fetch_all(MYSQLI_ASSOC);
 	} elseif ($tournament["eventType"] == "league") {
 		$matchDB = $dbcn->execute_query("SELECT * FROM matchups WHERE ((OPL_ID_team1 = ? AND OPL_ID_team2 = ?) OR (OPL_ID_team1 = ? AND OPL_ID_team2 = ?)) AND OPL_ID_tournament IN (SELECT OPL_ID FROM tournaments WHERE eventType = 'group' AND OPL_ID_parent = ?)", [$BlueTeamID, $RedTeamID, $RedTeamID, $BlueTeamID, $tournamentID])->fetch_all(MYSQLI_ASSOC);
