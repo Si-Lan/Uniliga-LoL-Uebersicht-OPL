4ce022a6ef501eda611cbfb3e8791dfde7167152
Si-Lan <online@simon-lang.de>
2024-01-21 00:59:39 +0100

    (now really) fixed opl get_players

--- admin/functions/get-opl-data.php
+++ admin/functions/get-opl-data.php
@@ -456,7 +456,7 @@ function get_players_for_team($teamID, $tournamentID):array {
 														    eventType='tournament'
 														        AND OPL_ID = ?
 														       )",
-		[$tournamentID, $tournamentID, $tournamentID, $tournamentID])->fetch_all(MYSQLI_ASSOC);
+		[$tournamentID, $tournamentID, $tournamentID, $tournamentID])->fetch_assoc();
 	$parent_tournamentID = $parent_tournament["OPL_ID"];
 	$tournament_end = strtotime($parent_tournament["dateEnd"]);
 
