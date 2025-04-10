840010511c7e985c55240521c337203fc75f5933
Si-Lan <online@simon-lang.de>
2025-04-10 23:47:59 +0200

    fix für verschwindende Elemente Admin Dashboard

--- admin/ajax/create_admin_buttons.php
+++ admin/ajax/create_admin_buttons.php
@@ -4,5 +4,6 @@ include_once dirname(__FILE__)."/../functions/fe-functions.php";
 $dbcn = create_dbcn();
 
 $open_accordeons = $_SERVER["HTTP_OPEN_ACCORDEONS"] ?? "[]";
+if ($open_accordeons == "") $open_accordeons = "[]";
 $open_accordeons = json_decode($open_accordeons);
 echo create_tournament_buttons($dbcn, $open_accordeons);
