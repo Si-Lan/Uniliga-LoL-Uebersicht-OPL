af3463a63d77649d7a3571a8c03eaa5b15d11f4c
Si-Lan <online@simon-lang.de>
2025-05-03 16:52:36 +0200

    Player und PlayerRepository Klassen hinzugefügt

---
+++ src/Entity/Player.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity;
+
+class Player {
+	/**
+	 * @param array<string> $matchesGotten
+	 */
+	public function __construct(
+		public int $id,
+		public string $name,
+		public ?string $riotIdName,
+		public ?string $riotIdTag,
+		public ?string $summonerName,
+		public ?string $summonerId,
+		public ?string $puuid,
+		public ?string $rankTier,
+		public ?string $rankDiv,
+		public ?string $rankLp,
+		public array $matchesGotten
+	) {}
+
+	public function getFullRiotID(): string {
+		return $this->riotIdName."#".$this->riotIdTag;
+	}
+	public function getEncodedRiotID(): string {
+		return urlencode($this->riotIdTag??"")."-".urlencode($this->riotIdTag??"");
+	}
+	public function getRiotIdTagWithPrefix(): string {
+		return (($this->riotIdTag??"") != "") ? "#".$this->riotIdTag : "";
+	}
+}
---
+++ src/Repository/PlayerRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repository;
+
+use App\Database\DatabaseConnection;
+use App\Entity\Player;
+
+class PlayerRepository {
+	private \mysqli $dbcn;
+
+	public function __construct() {
+		$this->dbcn = DatabaseConnection::getConnection();
+	}
+
+	public function findById(int $playerId): ?Player {
+		$result = $this->dbcn->execute_query("SELECT * FROM players WHERE OPL_ID = ?", [$playerId]);
+		$data = $result->fetch_assoc();
+
+		$player = $data ? new Player(
+			id: $data['OPL_ID'],
+			name: $data['name'],
+			riotIdName: $data['riotID_name'],
+			riotIdTag: $data['riotID_tag'],
+			summonerName: $data['summonerName'],
+			summonerId: $data['summonerID'],
+			puuid: $data['PUUID'],
+			rankTier: $data['rank_tier'],
+			rankDiv: $data['rank_div'],
+			rankLp: $data['rank_LP'],
+			matchesGotten: json_decode($data['matches_gotten']),
+		) : null;
+
+		return $player;
+	}
+}
