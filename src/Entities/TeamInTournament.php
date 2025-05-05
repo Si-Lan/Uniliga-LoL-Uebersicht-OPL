95ca99a1c36529f0dde0c5d52cb3b26a4389de18
Si-Lan <online@simon-lang.de>
2025-05-05 12:18:04 +0200

    Fix für möglich null Attribute in TeamInTournament Entities

--- src/Entities/TeamInTournament.php
+++ src/Entities/TeamInTournament.php
@@ -12,21 +12,21 @@ class TeamInTournament {
 	 * @param array<string, int> $champsBanned
 	 * @param array<string, int> $champsPlayedAgainst
 	 * @param array<string, int> $champsBannedAgainst
-	 * @param int $gamesPlayed
-	 * @param int $gamesWon
-	 * @param int $avgWinTime
+	 * @param int|null $gamesPlayed
+	 * @param int|null $gamesWon
+	 * @param int|null $avgWinTime
 	 * @param array<RankAverage> $ranks
 	 */
 	public function __construct(
 		public Team $team,
 		public Tournament $tournament,
-		public array $champsPlayed,
-		public array $champsBanned,
-		public array $champsPlayedAgainst,
-		public array $champsBannedAgainst,
-		public int $gamesPlayed,
-		public int $gamesWon,
-		public int $avgWinTime,
+		public ?array $champsPlayed,
+		public ?array $champsBanned,
+		public ?array $champsPlayedAgainst,
+		public ?array $champsBannedAgainst,
+		public ?int $gamesPlayed,
+		public ?int $gamesWon,
+		public ?int $avgWinTime,
 		public array $ranks
 	) {}
 }
--- src/Entities/TeamInTournamentStage.php
+++ src/Entities/TeamInTournamentStage.php
@@ -6,13 +6,13 @@ class TeamInTournamentStage {
 	public function __construct(
 		public Team $team,
 		public Tournament $tournamentStage,
-		public int $standing,
-		public int $played,
-		public int $wins,
-		public int $draws,
-		public int $losses,
-		public int $points,
-		public int $singleWins,
-		public int $singleLosses
+		public ?int $standing,
+		public ?int $played,
+		public ?int $wins,
+		public ?int $draws,
+		public ?int $losses,
+		public ?int $points,
+		public ?int $singleWins,
+		public ?int $singleLosses
 	) {}
 }
