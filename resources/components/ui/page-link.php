f1cfba914ae981711668aeae18f050ba35261e2b
Si-Lan <online@simon-lang.de>
2025-05-07 11:41:36 +0200

    PageLink UI Komponente hinzugefügt

---
+++ resources/components/ui/page-link.php
@@ -0,0 +1,4 @@
+<a href="<?=$this->href?>" class="<?=$this->classes?>">
+	<span class="link-text"><?=$this->text?></span>
+    <?=\App\Components\Helpers\IconRenderer::getMaterialIconSpan($this->linkIcon,["page-link-icon"])?>
+</a>
---
+++ src/Components/UI/PageLink.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Components\UI;
+
+class PageLink {
+	public string $classes;
+	public function __construct(
+		public string $href,
+		public string $text,
+		array $additionalClasses = [],
+		public string $linkIcon = 'chevron_right'
+	) {
+		$this->classes = implode(' ', array_filter(["page-link", ...$additionalClasses]));
+	}
+
+	public function render(): string {
+		ob_start();
+		include BASE_PATH.'/resources/components/ui/page-link.php';
+		return ob_get_clean();
+	}
+
+	public function __toString(): string {
+		return $this->render();
+	}
+}
