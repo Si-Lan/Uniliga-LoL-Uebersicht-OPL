b7dfc28a1cb1fe4d31201c7115a8479d809282a4
Si-Lan <online@simon-lang.de>
2025-05-12 15:42:45 +0200

    group-details soweit möglich an neue Struktur angepasst

--- public/pages/group-details.php
+++ public/pages/group-details.php
@@ -1,51 +1,25 @@
 <?php
 /** @var mysqli $dbcn  */
 
+use App\Components\Helpers\IconRenderer;
 use App\Components\Matches\MatchButtonList;
 use App\Components\Standings\StandingsTable;
+use App\Enums\EventFormat;
+use App\Enums\EventType;
 use App\Repositories\TournamentRepository;
+use App\Utilities\UserPreferences;
 
 $tournamentRepo = new TournamentRepository();
 
-$tournament_url_path = $_GET["tournament"] ?? NULL;
-$groupID= $_GET["group"] ?? NULL;
+$group = $tournamentRepo->findStandingsEventById($_GET["group"]);
 
-$groupObj = $tournamentRepo->findById($groupID);
-
-$tournamentID = $tournament_url_path;
-if (preg_match("/^(winter|sommer)([0-9]{2})$/",strtolower($tournamentID),$url_path_matches)) {
-	$split = $url_path_matches[1];
-	$season = $url_path_matches[2];
-	$tournamentID = $dbcn->execute_query("SELECT OPL_ID FROM tournaments WHERE season = ? AND split = ? AND eventType = 'tournament'", [$season, $split])->fetch_column();
-}
-$tournament = $dbcn->execute_query("SELECT * FROM tournaments WHERE OPL_ID = ? AND eventType = 'tournament'", [$tournamentID])->fetch_assoc();
-$group = $dbcn->execute_query("SELECT * FROM tournaments WHERE OPL_ID = ? AND eventType = 'group'", [$groupID])->fetch_assoc();
-if ($group == null) {
-    $group = $dbcn->execute_query("SELECT * FROM tournaments WHERE OPL_ID = ? AND eventType = 'league' AND format = 'swiss'", [$groupID])->fetch_assoc();
-}
-$swiss = (($group["format"]??"") == 'swiss');
-
-if ($tournament == NULL || $group == NULL) {
-	$_GET["error"] = "404";
-	$_GET["404type"] = "group";
-	$_GET["groupid"] = $groupID;
-	require "error.php";
-	echo "</html>";
-	exit();
-}
-
-if ($swiss) {
+if ($group->eventType === EventType::LEAGUE) {
     $league = $group;
 } else {
-	$league = $dbcn->execute_query("SELECT * FROM tournaments WHERE OPL_ID = ? AND eventType = 'league'", [$group["OPL_ID_parent"]])->fetch_assoc();
+	$league = $group->directParentTournament;
 }
 
-$t_name_clean = preg_replace("/LoL\s/i","",$tournament["name"]);
-if ($swiss) {
-	echo create_html_head_elements(css: ["game"], title: "Liga {$league["number"]} - Swiss-Gruppe | $t_name_clean | Uniliga LoL - Übersicht");
-} else {
-	echo create_html_head_elements(css: ["game"], title: "Liga {$league["number"]} - Gruppe {$group["number"]} | $t_name_clean | Uniliga LoL - Übersicht");
-}
+echo create_html_head_elements(css: ['game'], title: $group->getFullName()." | ".$group->rootTournament->getShortName()." | Uniliga LoL - Übersicht");
 
 $open_popup = "";
 if (isset($_GET['match'])) {
@@ -53,31 +27,19 @@ if (isset($_GET['match'])) {
 }
 
 ?>
-<body class="group <?=is_light_mode(true)." $open_popup"?>">
+<body class="group <?= UserPreferences::getLightModeClass()." $open_popup"?>">
 <?php
 
-$pageurl = $_SERVER['REQUEST_URI'];
-$opl_tourn_url = "https://www.opleague.pro/event/";
+$oplUrlEvent = "https://www.opleague.pro/event/";
 
-$teams_from_groupDB = $dbcn->execute_query("SELECT * FROM teams JOIN teams_in_tournaments tit ON teams.OPL_ID = tit.OPL_ID_team WHERE tit.OPL_ID_group = ? ORDER BY standing",[$group['OPL_ID']])->fetch_all(MYSQLI_ASSOC);
-$teams_from_group = [];
-foreach ($teams_from_groupDB as $i=>$team_from_group) {
-	$teams_from_group[$team_from_group['OPL_ID']] = array("name"=>$team_from_group['name'], "OPL_ID_logo"=>$team_from_group['OPL_ID_logo']);
-}
-if ($league["format"] === "swiss") {
-	$group_title = "Swiss-Gruppe";
-} else {
-	$group_title = "Gruppe {$group['number']}";
-}
+echo create_header(dbcn: $dbcn, title: "tournament", tournament_id: $group->rootTournament->id);
 
-echo create_header(dbcn: $dbcn, title: "tournament", tournament_id: $tournamentID);
+echo create_tournament_nav_buttons($group->rootTournament->id, $dbcn,"group",$league->id,$group->id);
 
-echo create_tournament_nav_buttons($tournamentID, $dbcn,"group",$league['OPL_ID'],$groupID);
+if (!$group->rootTournament->archived) {
 
-if (!$tournament["archived"]) {
-
-	$last_user_update = $dbcn->execute_query("SELECT last_update FROM updates_user_group WHERE OPL_ID_group = ?", [$groupID])->fetch_column();
-	$last_cron_update = $dbcn->execute_query("SELECT last_update FROM updates_cron WHERE OPL_ID_tournament = ?", [$tournamentID])->fetch_column();
+	$last_user_update = $dbcn->execute_query("SELECT last_update FROM updates_user_group WHERE OPL_ID_group = ?", [$group->id])->fetch_column();
+	$last_cron_update = $dbcn->execute_query("SELECT last_update FROM updates_cron WHERE OPL_ID_tournament = ?", [$group->rootTournament->id])->fetch_column();
 
 	$last_update = max($last_user_update, $last_cron_update);
 
@@ -90,25 +52,32 @@ if (!$tournament["archived"]) {
 	}
 }
 
-echo "<div class='pagetitlewrapper withupdatebutton'>
-				<div class='pagetitle'>
-					<h2 class='pagetitle'>Liga {$league['number']} - $group_title</h2>
-                	<a href='$opl_tourn_url{$groupID}' target='_blank' class='opl-link'><div class='material-symbol'>".file_get_contents(__DIR__."/../icons/material/open_in_new.svg")."</div></a>
-              	</div>";
-if (!$tournament["archived"]) {
-	echo "
-              	<div class='updatebuttonwrapper'>
-              		<button type='button' class='user_update user_update_group update_data material-symbol' data-group='$groupID'><div class='material-symbol'>" . file_get_contents(__DIR__ . "/../icons/material/sync.svg") . "</div></button>
-					<span class='last-update'>letztes Update:<br>$updatediff</span>
-				</div>";
+if ($league->format === EventFormat::SWISS) {
+	$group_title = "Swiss-Gruppe";
+} else {
+	$group_title = $group->getShortName();
 }
-echo "
-              </div>";
-
-echo "<main>";
-echo new StandingsTable($groupObj);
-echo new MatchButtonList($groupObj);
-echo "</main>";
-
 ?>
+    <div class='pagetitlewrapper withupdatebutton'>
+	    <div class='pagetitle'>
+		    <h2 class='pagetitle'><?= $league->getFullName() ?> - <?= $group_title ?></h2>
+                <a href='<?= $oplUrlEvent.$group->id?>' target='_blank' class='opl-link'><?= IconRenderer::getMaterialIconDiv('open_in_new') ?></a>
+        </div>
+    <?php
+if (!$group->rootTournament->archived) {
+    ?>
+        <div class='updatebuttonwrapper'>
+            <button type='button' class='user_update user_update_group update_data material-symbol' data-group='<?= $group->id ?>'><?= IconRenderer::getMaterialIconSpan('sync') ?></button>
+			    <span class='last-update'>letztes Update:<br><?= $updatediff ?></span>
+        </div>
+    <?php
+}
+?>
+    </div>
+    <main>
+        <?php
+    echo new StandingsTable($group);
+    echo new MatchButtonList($group);
+    ?>
+    </main>
 </body>
