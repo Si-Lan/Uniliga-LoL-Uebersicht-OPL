c0c5ddecccc8dea780bc3978839564d93730d263
Si-Lan <online@simon-lang.de>
2025-05-13 10:04:36 +0200

    UpdateHistory Entity und UpdateButton Komponente hinzugefügt

---
+++ src/Components/UpdateButton.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Components;
+
+use App\Entities\Matchup;
+use App\Entities\TeamInTournament;
+use App\Entities\Tournament;
+use App\Entities\UpdateHistory;
+use App\Repositories\UpdateHistoryRepository;
+
+class UpdateButton {
+	private UpdateHistory $updateHistory;
+	private string $htmlDataString = '';
+	private string $htmlClassString = '';
+	public function __construct(
+		public Tournament|TeamInTournament|Matchup $entity
+	) {
+		$updateHistoryRepo = new UpdateHistoryRepository();
+		if ($entity instanceof Tournament) {
+			$this->htmlClassString = 'user_update_group';
+			$this->htmlDataString = "data-group='$entity->id'";
+			$this->updateHistory = $updateHistoryRepo->findByTournamentStage($entity);
+		}
+		if ($entity instanceof TeamInTournament) {
+			$this->htmlClassString = 'user_update_team';
+			$this->htmlDataString = "data-team='{$entity->tournament->id}'";
+			$this->updateHistory = $updateHistoryRepo->findByTeamInTournament($entity);
+		}
+		if ($entity instanceof Matchup) {
+			$this->htmlClassString = 'user_update_match';
+			$this->htmlDataString = "data-match='$entity->id'";
+			$this->updateHistory = $updateHistoryRepo->findByMatchup($entity);
+		}
+	}
+
+	public function render(): string {
+		$htmlDataString = $this->htmlDataString;
+		$htmlClassString = $this->htmlClassString;
+		$updatediff = $this->updateHistory->getLastUpdateString();
+		ob_start();
+		include BASE_PATH.'/resources/components/update-button.php';
+		return ob_get_clean();
+	}
+	public function __toString(): string {
+		return $this->render();
+	}
+}
---
+++ src/Repositories/UpdateHistoryRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Entities\Matchup;
+use App\Entities\TeamInTournament;
+use App\Entities\Tournament;
+use App\Entities\UpdateHistory;
+use App\Enums\EventType;
+use App\Utilities\DataParsingHelpers;
+
+class UpdateHistoryRepository extends AbstractRepository {
+	use DataParsingHelpers;
+	public function mapToEntity(array $data, Tournament|TeamInTournament|Matchup $entity): UpdateHistory {
+		return new UpdateHistory(
+			entity: $entity,
+			lastUpdate: $this->DateTimeImmutableOrNull($data['last_update']),
+			lastCronUpdate: $this->DateTimeImmutableOrNull($data['last_cron_update']),
+		);
+	}
+
+	public function findByTournamentStage(Tournament $tournamentStage): ?UpdateHistory {
+		if ($tournamentStage->eventType === EventType::TOURNAMENT) return null;
+		$query = 'SELECT t.OPL_ID, uu.last_update, c.last_update as last_cron_update FROM tournaments t LEFT JOIN updates_user_group uu ON t.OPL_ID = uu.OPL_ID_group LEFT JOIN updates_cron c ON c.OPL_ID_tournament = t.OPL_ID_top_parent WHERE t.OPL_ID = ?';
+		$result = $this->dbcn->execute_query($query, [$tournamentStage->id]);
+		$data = $result->fetch_assoc();
+
+		return $data ? $this->mapToEntity($data, $tournamentStage) : null;
+	}
+	public function findByTeamInTournament(TeamInTournament $team): ?UpdateHistory {
+		$query = 'SELECT t.OPL_ID, uu.last_update, c.last_update as last_cron_update FROM teams t LEFT JOIN updates_user_team uu ON t.OPL_ID = uu.OPL_ID_team LEFT JOIN updates_cron c ON c.OPL_ID_tournament = ? WHERE t.OPL_ID = ?';
+		$result = $this->dbcn->execute_query($query, [$team->tournament->id, $team->team->id]);
+		$data = $result->fetch_assoc();
+
+		return $data ? $this->mapToEntity($data, $team) : null;
+	}
+	public function findByMatchup(Matchup $matchup): ?UpdateHistory {
+		$query = 'SELECT m.OPL_ID, uu.last_update, c.last_update as last_cron_update FROM matchups m LEFT JOIN updates_user_matchup uu ON m.OPL_ID = uu.OPL_ID_matchup LEFT JOIN updates_cron c ON c.OPL_ID_tournament = ? WHERE m.OPL_ID = ?';
+		$result = $this->dbcn->execute_query($query, [$matchup->tournamentStage->rootTournament->id, $matchup->id]);
+		$data = $result->fetch_assoc();
+
+		return $data ? $this->mapToEntity($data, $matchup) : null;
+	}
+}
