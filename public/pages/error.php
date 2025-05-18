3ba75367e40480945b3201d4854eea5467f246f7
Si-Lan <online@simon-lang.de>
2025-05-18 10:08:43 +0200

    error.php an neue Struktur angepasst

--- public/pages/error.php
+++ public/pages/error.php
@@ -1,5 +1,8 @@
 <?php
-require_once dirname(__DIR__,2)."/src/functions/fe-functions.php";
+
+use App\Components\Navigation\Header;
+use App\Enums\HeaderType;
+use App\Page\PageMeta;
 
 $errortype = $_GET["error"] ?? NULL;
 
@@ -7,58 +10,60 @@ switch($errortype) {
 	case "404":
         $type404 = $_GET["404type"] ?? null;
 		http_response_code(404);
+		$headerType = HeaderType::NOT_FOUND;
         switch ($type404) {
             case "tournament":
 				$errortext = "Kein Turnier unter der angegebenen ID gefunden!";
-				echo create_html_head_elements(title: "404 - Turnier nicht gefunden | Uniliga LoL - Übersicht");
+				$title = "404 - Turnier nicht gefunden";
 				break;
 			case "group":
 				$errortext = "Keine Gruppe unter der angegebenen ID gefunden!";
-				echo create_html_head_elements(title: "404 - Gruppe nicht gefunden | Uniliga LoL - Übersicht");
+				$title = "404 - Gruppe nicht gefunden";
 				break;
 			case "wildcard":
 				$errortext = "Kein Wildcard-Turnier unter der angegebenen ID gefunden!";
-				echo create_html_head_elements(title: "404 - Wildcard-Turnier nicht gefunden | Uniliga LoL - Übersicht");
+				$title = "404 - Wildcard-Turnier nicht gefunden";
 				break;
 			case "playoffs":
 				$errortext = "Keine Playoffs unter der angegebenen ID gefunden!";
-				echo create_html_head_elements(title: "404 - Playoffs nicht gefunden | Uniliga LoL - Übersicht");
+				$title = "404 - Playoffs nicht gefunden";
 				break;
 			case "team":
 				$errortext = "Kein Team unter der angegebenen ID gefunden!";
-				echo create_html_head_elements(title: "404 - Team nicht gefunden | Uniliga LoL - Übersicht");
+				$title = "404 - Team nicht gefunden";
 				break;
             case "team-in-tournament":
                 $errortext = "Dieses Team spielt nicht im angegebenen Turnier";
-                echo create_html_head_elements(title: "404 - Team nicht im Turnier | Uniliga LoL - Übersicht");
+				$title = "404 - Team nicht im Turnier";
                 break;
 			case "player":
                 $errortext = "Kein Spieler unter der angegebenen ID gefunden!";
-				echo create_html_head_elements(title: "404 - Spieler nicht gefunden | Uniliga LoL - Übersicht");
+				$title = "404 - Spieler nicht gefunden";
                 break;
 			default:
 				$errortext = "Die gesuchte Seite wurde nicht gefunden";
-				echo create_html_head_elements(title: "404 - Seite nicht gefunden | Uniliga LoL - Übersicht");
+				$title = "404 - Seite nicht gefunden";
                 break;
 		}
 		break;
     case "db":
         http_response_code(500);
-        $errortype = "error";
+        $headerType = HeaderType::ERROR;
         $errortext = "Fehler bei der Datenbankverbindung!";
-		echo create_html_head_elements(title: "Fehler | Uniliga LoL - Übersicht");
+		$title = "Fehler";
         break;
 	default:
-		$errortype = "error";
+		$headerType = HeaderType::ERROR;
 		$errortext = "Ein unbekannter Fehler ist aufgetreten";
-		echo create_html_head_elements(title: "Fehler | Uniliga LoL - Übersicht");
+		$title = "Fehler";
         break;
 }
 
+$pageMeta = new PageMeta($title, bodyClass: 'error');
+
 ?>
-<body class="error <?=is_light_mode(true)?>">
-    <?=create_header(title: $errortype)?>
-    <div style='text-align: center'>
-        <?=$errortext?>
-    </div>
-</body>
+
+<?= new Header($headerType)?>
+<div style='text-align: center'>
+    <?=$errortext?>
+</div>
