8d88c47afd36458c53c3cb529cc58e11b0b9bbe7
Si-Lan <online@simon-lang.de>
2025-05-23 10:33:33 +0200

    GameInMatch kann winning und losing Team ausgeben

--- src/Entities/GameInMatch.php
+++ src/Entities/GameInMatch.php
@@ -10,4 +10,23 @@ class GameInMatch {
 		public ?TeamInTournament $redTeam,
 		public bool $oplConfirmed
 	) {}
+
+	public function getWinningTeam(): ?TeamInTournament {
+		if ($this->game->gameData->blueTeamWin) {
+			return $this->blueTeam;
+		} elseif ($this->game->gameData->redTeamWin) {
+			return $this->redTeam;
+		} else {
+			return null;
+		}
+	}
+	public function getLosingTeam(): ?TeamInTournament {
+		if ($this->game->gameData->blueTeamWin) {
+			return $this->redTeam;
+		} elseif ($this->game->gameData->redTeamWin) {
+			return $this->blueTeam;
+		} else {
+			return null;
+		}
+	}
 }
