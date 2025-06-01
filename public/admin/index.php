2cce54e18aa0f2634ca8bc980caec5c0fa5eca6b
Si-Lan <online@simon-lang.de>
2025-06-01 18:52:43 +0200

    Referenz auf alte Funktionen entfernt

--- public/admin/index.php
+++ public/admin/index.php
@@ -3,7 +3,6 @@
 require_once dirname(__DIR__,2)."/bootstrap.php";
 
 include_once dirname(__DIR__,2)."/config/data.php";
-include_once dirname(__DIR__,2)."/src/old_functions/fe-functions.php";
 include_once dirname(__DIR__,2)."/src/old_functions/helper.php";
 include_once dirname(__DIR__,2)."/src/old_functions/admin/fe-functions.php";
 include_once dirname(__DIR__,2)."/src/old_functions/admin/ddragon-update.php";
--- public/index.php
+++ public/index.php
@@ -3,6 +3,5 @@
 require_once dirname(__DIR__).'/bootstrap.php';
 
 include_once BASE_PATH."/config/data.php";
-include_once BASE_PATH."/src/old_functions/fe-functions.php";
 
 \App\Core\Router::handle('pages');
