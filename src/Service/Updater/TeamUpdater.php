2ebe2f630dfa69c9a73392ccb0681213fe795460
Si-Lan <online@simon-lang.de>
2025-10-16 14:48:00 +0200

    TeamUpdate Funktionalität von TeamsHandler in TeamUpdater umgezogen

---
+++ src/Service/Updater/TeamUpdater.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Service\Updater;
+
+use App\Domain\Repositories\PlayerInTeamInTournamentRepository;
+use App\Domain\Repositories\PlayerInTeamRepository;
+use App\Domain\Repositories\PlayerRepository;
+use App\Domain\Repositories\TeamInTournamentRepository;
+use App\Domain\Repositories\TeamRepository;
+use App\Domain\Repositories\TournamentRepository;
+use App\Service\OplApiService;
+
+class TeamUpdater {
+	private TeamRepository $teamRepo;
+	private OplApiService $oplApiService;
+	public function __construct() {
+		$this->teamRepo = new TeamRepository();
+		$this->oplApiService = new OplApiService();
+	}
+
+	/**
+	 * @throws \Exception
+	 */
+	public function updatePlayers(int $teamId): array {
+		$team = $this->teamRepo->findById($teamId);
+		if ($team === null) {
+			throw new \Exception("Team not found", 404);
+		}
+
+		try {
+			$teamData = $this->oplApiService->fetchFromEndpoint("team/$teamId/users");
+		} catch (\Exception $e) {
+			throw new \Exception("Failed to fetch data from OPL API: ".$e->getMessage(), 500);
+		}
+
+		$oplPlayers = $teamData['users'];
+		$ids = array_column($oplPlayers, 'ID');
+
+		$playerRepo = new PlayerRepository();
+		$playerInTeamRepo = new PlayerInTeamRepository();
+		$playerinTeamInTournamentRepo = new PlayerInTeamInTournamentRepository();
+		$tournamentRepo = new TournamentRepository();
+		$teamInTournamentRepo = new TeamInTournamentRepository();
+
+		$activeTournaments = $tournamentRepo->findAllRunningRootTournaments();
+
+		$tournamentAdditions = [];
+		foreach ($activeTournaments as $i=>$tournament) {
+			if (!$teamInTournamentRepo->isTeamInRootTournament($teamId, $tournament->id)) {
+				unset($activeTournaments[$i]);
+				continue;
+			}
+			$tournamentAdditions[$tournament->id] = ['tournament' => $tournament, 'addedPlayers' => [], 'removedPlayers' => []];
+		}
+		$activeTournaments = array_values($activeTournaments);
+
+		$saveResults = [];
+		$addedPlayers = [];
+		foreach ($oplPlayers as $oplPlayer) {
+			// Spieler speichern
+			$playerEntity = $playerRepo->createFromOplData($oplPlayer);
+			$saveResult = $playerRepo->save($playerEntity, fromOplData: true);
+			$saveResults[] = $saveResult;
+
+			// Spieler in Team eintragen
+			$addedToTeam = $playerInTeamRepo->addPlayerToTeam($playerEntity->id, $team->id);
+			if ($addedToTeam) {
+				$addedPlayers[] = $saveResult["player"];
+			}
+
+			// Spieler in Team in aktive Turniere eintragen
+			foreach ($activeTournaments as $tournament) {
+				$addedToTeamInTournament = $playerinTeamInTournamentRepo->addPlayerToTeamInTournament($playerEntity->id, $team->id, $tournament->id);
+				if ($addedToTeamInTournament) {
+					$tournamentAdditions[$tournament->id]['addedPlayers'][] = $saveResult["player"];
+				}
+			}
+		}
+
+		// Spieler in Team inaktiv setzen
+		$playersCurrentlyInTeam = $playerInTeamRepo->findAllByTeamAndActiveStatus($team, active: true);
+		$removedPlayers = [];
+		foreach ($playersCurrentlyInTeam as $playerInTeam) {
+			if (!in_array($playerInTeam->player->id, $ids)) {
+				$playerInTeamRepo->removePlayerFromTeam($playerInTeam->player->id, $team->id);
+				$removedPlayers[] = $playerInTeam->player;
+			}
+		}
+
+		// Spieler in Team in aktiven Turnieren inaktiv setzen
+		foreach ($activeTournaments as $tournament) {
+			$playersCurrentlyInTeamInTournament = $playerinTeamInTournamentRepo->findAllByTeamAndTournamentAndActiveStatus($team, $tournament, active: true);
+			foreach ($playersCurrentlyInTeamInTournament as $playerInTeamInTournament) {
+				if (!in_array($playerInTeamInTournament->player->id, $ids)) {
+					$playerinTeamInTournamentRepo->removePlayerFromTeamInTournament($playerInTeamInTournament->player->id, $team->id, $tournament->id);
+					$tournamentAdditions[$tournament->id]['removedPlayers'][] = $playerInTeamInTournament->player;
+				}
+			}
+		}
+		$tournamentAdditions = array_values($tournamentAdditions);
+
+		return ['players' => $saveResults, 'addedPlayers' => $addedPlayers, 'removedPlayers' => $removedPlayers, 'tournamentChanges' => $tournamentAdditions];
+	}
+}
