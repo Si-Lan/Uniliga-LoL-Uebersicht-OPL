9b1a8e5caec5311df98a37aa1903d33ccb4ec84b
Si-Lan <online@simon-lang.de>
2025-10-26 02:01:23 +0200

    Veraltete Code-Dateien gelöscht

--- public/admin/index.php
+++ public/admin/index.php
@@ -4,6 +4,5 @@ require_once dirname(__DIR__,2)."/bootstrap.php";
 
 include_once dirname(__DIR__,2)."/config/data.php";
 include_once dirname(__DIR__,2)."/src/old_functions/helper.php";
-include_once dirname(__DIR__,2)."/src/old_functions/admin/ddragon-update.php";
 
 \App\Core\Router::handle('pages');
